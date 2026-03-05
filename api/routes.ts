a9821155f035766f81fc909c4653843ce523d8b4
burger <abhish.72@gmail.com>
2026-03-06 01:37:39 +0530

    Fix 500: Add mandatory .js extensions to ESM imports in api/ directory

--- api/routes.ts
+++ api/routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { query } from './db';
+import { query } from './db.js';
 import { z } from 'zod';
 
 // Validation Schemas
